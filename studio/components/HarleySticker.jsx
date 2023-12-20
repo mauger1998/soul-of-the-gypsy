a9594e5e903a11093162f303e645a4239578302d
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2023-12-20 09:22:37 +0000

    Fix all browser errors and change all images to webp

--- studio/components/HarleySticker.jsx
+++ studio/components/HarleySticker.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function HarleySticker() {
-  return <img src="/static/pngegg (31) 1.png" alt="Harley Davidson Sticker" />
+  return <img src="/static/pngegg (31) 1.webp" alt="Harley Davidson Sticker" />
 }
 
 export default HarleySticker
--- studio/components/KeithRichardsSticker.jsx
+++ studio/components/KeithRichardsSticker.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function KeithRichardsSticker() {
   return (
     <div>
-      <img src="/static/71FpX231+0L 1.png" alt="Keith Richards Sticker" />
+      <img src="/static/71FpX231+0L 1.webp" alt="Keith Richards Sticker" />
     </div>
   )
 }
